9d7fbd40c16f61f6e0f4c1e2ebe323a6cd15f9aa
AlexTraveylan <monsieurdemares@gmail.com>
2024-07-09 22:51:44 +0200

    chore: Update npm dependencies and fix locale handling in page content

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,13 +1,13 @@
-import { getCurrentLocale, getI18n } from "../../locales/server"
+import { LanguagePossibles } from "@/lib/app-types"
+import { getI18n } from "../../locales/server"
 
-export default async function ({ params: { locale } }: { params: { locale: string } }) {
+export default async function ({ params: { locale } }: { params: { locale: LanguagePossibles } }) {
   const t = await getI18n()
 
-  const currentLocale = getCurrentLocale()
   return (
     <div>
       <h1>{t("hello")}</h1>
-      <p>{`-> ${currentLocale}`}</p>
+      <p>{`-> ${locale}`}</p>
     </div>
   )
 }
