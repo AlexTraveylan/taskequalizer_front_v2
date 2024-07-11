12b20b777a2a0fe87c8967937413e7f7bb73bda3
AlexTraveylan <monsieurdemares@gmail.com>
2024-07-11 16:45:28 +0200

    feat: Add LabelList to MultipleBarChart for displaying data labels
    The code changes in multipleBar.tsx introduce the addition of the LabelList component to the Bar component in the MultipleBarChart. This allows for the display of data labels on top of each bar in the chart. The labels are positioned at the top, with an offset of 12 pixels, and have a font size of 12 pixels. This enhancement improves the readability and understanding of the chart data.

--- components/taskequalizer/charts/multipleBar.tsx
+++ components/taskequalizer/charts/multipleBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts"
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
@@ -24,12 +24,20 @@ export function MultipleBarChart({ chartConfig, chartData, dataKey, nameKey, tit
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
-          <BarChart accessibilityLayer data={chartData}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 30,
+            }}
+          >
             <CartesianGrid vertical={false} />
             <XAxis dataKey={dataKey} tickLine={false} tickMargin={10} axisLine={false} tickFormatter={(value) => value.slice(0, 10)} />
             <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dashed" />} />
             {nameKey.map((nkey, index) => (
-              <Bar key={nkey} dataKey={nkey} fill={colors[index]} radius={4} />
+              <Bar key={nkey} dataKey={nkey} fill={colors[index]} radius={4}>
+                <LabelList position="top" offset={12} className="fill-foreground" fontSize={12} />
+              </Bar>
             ))}
           </BarChart>
         </ChartContainer>
