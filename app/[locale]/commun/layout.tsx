5b0aefe862e6a941d1798d4dee83f613bbc5b7ee
AlexTraveylan <monsieurdemares@gmail.com>
2024-07-15 16:25:16 +0200

    feat: Add Toaster component for displaying notifications
    Add the Toaster component from the "sonner" library to the CommunLayout component. This component will display notifications with rich colors in the top-left position. The addition of the Toaster enhances the user experience by providing a visual cue for important updates or messages.

--- app/[locale]/commun/layout.tsx
+++ app/[locale]/commun/layout.tsx
@@ -2,6 +2,7 @@
 
 import { LanguagePossibles } from "@/lib/app-types"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from "sonner"
 import { Provider } from "../provider"
 
 const queryClient = new QueryClient()
@@ -9,6 +10,7 @@ const queryClient = new QueryClient()
 export default function CommunLayout({ params: { locale }, children }: { params: { locale: LanguagePossibles }; children: React.ReactNode }) {
   return (
     <Provider locale={locale}>
+      <Toaster richColors position="top-left" />
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </Provider>
   )
