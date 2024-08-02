298363dc5fe3600929fea51249eabc423c686d20
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-02 12:55:10 +0200

    feat: Add lazy loading for images in possible tasks page

--- app/[locale]/commun/settings/possible-tasks/page.tsx
+++ app/[locale]/commun/settings/possible-tasks/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
 import { DisplayPossibleTasks } from "@/components/settings/display-p-tasks"
 import { PossibleTaskForm } from "@/components/settings/possible-task-form"
+import { taskEqualizerNavItems } from "@/lib/app-types"
+import { Rocket } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function PossibleTasksPage() {
+  const router = useRouter()
+
   return (
     <div className="flex flex-col gap-8">
+      <Rocket className="cursor-pointer" onClick={() => router.push(taskEqualizerNavItems.currentTask.href)} />
       <DisplayPossibleTasks />
       <PossibleTaskForm />
     </div>
