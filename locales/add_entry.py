97f166a9000fa68134a631527947d1260f6ec06d
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-08 17:29:47 +0200

    refactor: Add lazy loading for header logo image
    Add lazy loading functionality to the header logo image using the `next/image` component. This change improves page load performance by deferring the loading of the image until it is in the viewport, resulting in faster initial page rendering.

--- locales/add_entry.py
+++ locales/add_entry.py
@@ -1,25 +1,58 @@
 from pathlib import Path
+from secrets import OPENAI_API_KEY
 
-LANGUAGE_POSSIBLES = ["en", "fr", "de"]
+from openai import OpenAI
+
+LANGUAGE_POSSIBLES: dict[str, str] = {
+    "fr": "Français",
+    "en": "Anglais",
+    "de": "Allemand",
+}
+
+
+def translate(locale: str, sentence: str) -> str:
+    if locale == "fr":
+        return sentence
+
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": f"Ton rôle est de traduire les phrases qu'on te donne en {LANGUAGE_POSSIBLES[locale]}. Tu réponderas uniquement le texte traduit. Sans contexte, sans commentaire et sans explication.",
+            },
+            {
+                "role": "user",
+                "content": f"Traduit en {LANGUAGE_POSSIBLES[locale]} la phrase suivante : \n{sentence}",
+            },
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return chat_completion.choices[0].message.content
 
 
 def add_language_entry(new_key: str, new_value: str):
-    for language in LANGUAGE_POSSIBLES:
-        file_path = Path(__file__).parent / f"{language}.ts"
+    for locale in LANGUAGE_POSSIBLES:
+        file_path = Path(__file__).parent / f"{locale}.ts"
         if not file_path.exists():
-            print(f"Le fichier {language}.ts n'existe pas.")
+            print(f"Le fichier {locale}.ts n'existe pas.")
             continue
 
-        with open(file_path, "r") as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             content = file.read()
 
         # Vérifier si l'entrée existe déjà
         if f'"{new_key}":' in content:
-            print(f"L'entrée '{new_key}' existe déjà dans le fichier {language}.ts")
+            print(f"L'entrée '{new_key}' existe déjà dans le fichier {locale}.ts")
             continue
 
         # Trouver l'endroit où insérer la nouvelle entrée
         insert_index = content.rfind("}")
+
+        # Traduire la nouvelle valeur
+        new_value = translate(locale, new_value)
 
         # Ajouter la nouvelle entrée
         new_entry = f'  "{new_key}": "{new_value}",'
@@ -27,10 +60,10 @@ def add_language_entry(new_key: str, new_value: str):
             content[:insert_index] + new_entry + "\n" + content[insert_index:]
         )
 
-        with open(file_path, "w") as file:
+        with open(file_path, "w", encoding="utf-8") as file:
             file.write(updated_content)
 
-        print(f"Nouvelle entrée ajoutée dans {language}.ts : {new_key} = {new_value}")
+        print(f"Nouvelle entrée ajoutée dans {locale}.ts : {new_key} = {new_value}")
 
 
 if __name__ == "__main__":
