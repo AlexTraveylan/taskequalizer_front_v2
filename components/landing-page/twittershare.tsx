5f3ad4c7fee2688338dbc49ca45b2b9d1a5796c8
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-12 11:05:55 +0200

    fix hydratation

--- components/landing-page/twittershare.tsx
+++ components/landing-page/twittershare.tsx
@@ -1,40 +1,43 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
 import { Facebook, Linkedin, MessageCircleHeart, Twitter } from "lucide-react"
 import { FacebookShareButton, LinkedinShareButton, PinterestShareButton, TwitterShareButton } from "react-share"
 
 export const Share = ({ shareText, shareTitle }: { shareText: string; shareTitle: string }) => {
   return (
-    <div className="flex flex-wrap gap-4 justify-center">
-      <TwitterShareButton url="https://taskequalizer.vercel.app" title={shareTitle} className="text-primary-foreground">
-        <Button className="px-8 py-6">
-          <Twitter size={32} />
-          <span className="ml-2 ">{shareText} Twitter</span>
-        </Button>
+    <div className="flex flex-wrap gap-5 justify-center">
+      <TwitterShareButton
+        url="https://taskequalizer.vercel.app"
+        title={shareTitle}
+        className="px-8 py-6 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium"
+      >
+        <Twitter size={32} />
+        <span className="ml-2 ">{shareText} Twitter</span>
       </TwitterShareButton>
-      <FacebookShareButton url="https://taskequalizer.vercel.app" title={shareTitle} className="text-primary-foreground">
-        <Button className="px-8 py-6">
-          <Facebook size={32} />
-          <span className="ml-2 ">{shareText} Facebook</span>
-        </Button>
+      <FacebookShareButton
+        url="https://taskequalizer.vercel.app"
+        title={shareTitle}
+        className="px-8 py-6 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium"
+      >
+        <Facebook size={32} />
+        <span className="ml-2 ">{shareText} Facebook</span>
       </FacebookShareButton>
       <PinterestShareButton
         url="https://taskequalizer.vercel.app"
         title={shareTitle}
-        className="text-primary-foreground"
+        className="px-8 py-6 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium"
         media="https://taskequalizer.vercel.app/images/couple_landing_page.png"
       >
-        <Button className="px-8 py-6">
-          <MessageCircleHeart size={32} />
-          <span className="ml-2 ">{shareText} Pinterest</span>
-        </Button>
+        <MessageCircleHeart size={32} />
+        <span className="ml-2 ">{shareText} Pinterest</span>
       </PinterestShareButton>
-      <LinkedinShareButton url="https://taskequalizer.vercel.app" title={shareTitle} className="text-primary-foreground">
-        <Button className="px-8 py-6">
-          <Linkedin size={32} />
-          <span className="ml-2 ">{shareText} Linkedin</span>
-        </Button>
+      <LinkedinShareButton
+        url="https://taskequalizer.vercel.app"
+        title={shareTitle}
+        className="px-8 py-6 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium"
+      >
+        <Linkedin size={32} />
+        <span className="ml-2 ">{shareText} Linkedin</span>
       </LinkedinShareButton>
     </div>
   )
