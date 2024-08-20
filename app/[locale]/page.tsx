b858fe1cc7a8061523950eb8e0cabeab3c08a616
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-20 12:19:36 +0200

    adding product hunt

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,6 +10,20 @@ export default async function Home({ params: { locale } }: { params: { locale: L
     <div className="flex flex-col gap-8">
       <BelowTheFold />
       <Share shareText={scopedT("share")} shareTitle={scopedT("share-title")} />
+      <div className="flex justify-center">
+        <a
+          href="https://www.producthunt.com/posts/task-equalizer?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-task&#0045;equalizer"
+          target="_blank"
+        >
+          <img
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=479274&theme=dark"
+            alt="Task&#0032;Equalizer - Track&#0032;and&#0032;balance&#0032;household&#0032;chores&#0032;with&#0032;Task&#0032;Equalizer&#0046; | Product Hunt"
+            style={{ width: "250px", height: "54px" }}
+            width="250"
+            height="54"
+          />
+        </a>
+      </div>
     </div>
   )
 }
